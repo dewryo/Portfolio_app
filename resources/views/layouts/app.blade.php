e64bef6467b119647775d8c1f0e6dacbf520f2f0
dew <wvnm370god@yahoo.co.jp>
2023-09-16 17:54:14 +0900

    認証ボタン追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ChalkTalk</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite([ 'resources/js/app.js'])
 </head>
 <body>
     <header>
         <h1>ChalkTalk</h1>
+        <div class="header-buttons">
+        <a href="#" class="custom-link"><p>新規登録</p></a>
+        <a href="#" class="custom-link"><p>ログイン</p></a>
+        </div>
     </header>
 
     <main>

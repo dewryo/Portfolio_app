a8a7e3d8c11f470dc3c9e4a055bf7b368cc7954d
hatai <wvnm370god@yahoo.co.jp>
2023-11-18 09:43:10 +0900

    見た目修正

--- resources/js/components/SortButton.vue
+++ resources/js/components/SortButton.vue
@@ -1,13 +1,20 @@
 <template>
+<div class="row">
+    <div class="col-md-8 offset-md-5">
+        <form @submit.prevent="sortPosts" class="text-right">
+            <span class="sort-label">並べ替え：</span>
+            <a href="#" class="sort-link" @click.prevent="sort_new_Posts">新着順</a>
+            <a href="#" class="sort-link" @click.prevent="sort_like_Posts">いいね順</a>
+        </form>
+    </div>
+    <div class="col-md-8 ">
+        <hr>
+        </div>
 
-            <div class="col-md-8">
-                <form @submit.prevent="sortPosts">
-                    <a href="#" @click.prevent="sort_new_Posts">新着順</a>
-                    <a href="#" @click.prevent="sort_like_Posts">いいね順</a>
-                </form>
-            </div>
-        
+</div>
+    
 </template>
+
 
 <script setup>
 import { ref } from 'vue';
@@ -38,3 +45,21 @@ const sort_like_Posts = async () => {
   emit('update-posts',response.data);
 };
 </script>
+
+<style>
+.sort-label {
+    font-weight: bold;
+    margin-right: 10px;
+}
+
+.sort-link {
+    color: inherit; /* リンクの色を親要素と同じにする */
+    text-decoration: none; /* 下線を消す */
+    margin-right: 10px; /* 右のマージンを設定 */
+    transition: color 0.3s; /* 色の変化にアニメーションを追加 */
+}
+
+.sort-link:hover {
+    color: #007bff; /* ホバー時の色を変更 */
+}
+</style>

6968990a7452c28c8b56338eed4c12fcccf234e5
hatai <wvnm370god@yahoo.co.jp>
2023-12-16 10:24:42 +0900

    本番環境用にvite.config.jsを変更

--- vite.config.js
+++ vite.config.js
@@ -2,37 +2,36 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-
-export default defineConfig({
-    
-    resolve: {
-        alias: {
-          vue: 'vue/dist/vue.esm-bundler.js'
-        }
-      },
-
+export default defineConfig(({ command }) => ({
     plugins: [
-        
         vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-    
     ],
-    server: {
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
+    build: {
+        // 本番ビルドの設定
+        minify: 'terser', // または 'esbuild' を使用する
+        terserOptions: {
+            compress: {
+                drop_console: true, // コンソールログを削除
+            },
+        },
+    },
+    server: command === 'serve' ? {
+        // 開発環境の設定
         host: true,
         hmr: {
             host: 'localhost',
         },
         watch: {
             usePolling: true,
-          },
-    },
-
-    
-
-});
-
-
-
+        },
+    } : undefined, // 本番環境ではサーバ設定を無効化
+}));

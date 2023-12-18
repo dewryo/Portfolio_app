58c4b07c1bc5b18e6af06add9acaeee59732b069
hatai <wvnm370god@yahoo.co.jp>
2023-12-18 18:47:54 +0900

    トップページの投稿一覧画像をS3からの画像で表示するように変更

--- resources/js/components/PostCard.vue
+++ resources/js/components/PostCard.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="col-md-7">
+    <!-- 以下のコンテンツは変更なし -->
     <div class="card-body">
       <div @click="navigateToPost(post.id)" class="post-item">
         <h5 class="card-title">{{ truncate(post.title, 30) }}</h5>
+        <!-- タグの表示部分 -->
         <div v-if="post.tags.length">
           <span v-for="tag in post.tags" :key="tag.id" class="badge badge-primary" style="background-color: rgb(92, 122, 255); color: white; margin-right: 8px;">
             {{ tag.name }}
@@ -11,36 +13,36 @@
         <div v-else>
           <p>No tag available</p>
         </div>
-        <div style="height: 10px;"></div>
-        
+
+        <!-- 画像表示部分をS3のバケット名に合わせて更新 -->
         <div v-if="post.images.length" style="display: flex; justify-content: center; align-items: center; height: 200px;">
-            <img :src="post.images[0].file_path" :alt="post.images[0].file_name" class="img-fluid" style="max-height: 100%; max-width: 100%;">
+          <img :src="getS3ImageUrl(post.images[0].file_path)" :alt="post.images[0].file_name" class="img-fluid" style="max-height: 100%; max-width: 100%;">
         </div>
         <div v-else>
-            <p>No image available</p>
+          <p>No image available</p>
         </div>
-            
+
         <p class="card-text">{{ truncate(post.content, 60) }}</p>
-       </div>
+      </div>
+
+      <!-- ユーザーと関連する部分は変更なし -->
       <div class="d-flex justify-content-between align-items-center">
         <div>
           <LikeButton :post="post"/>
           <SavePostButton :postId="post.id" :is-saved="post.is_saved_by_user" />
         </div>
         <div @click="navigateToUser(post.user.id)" class="post-item">
-        <div class="d-flex align-items-center">
-        
-        <div>
-          <img v-if="post.user.profile_image" 
-              :src="`/storage/profile_images/${post.user.profile_image}`" 
-              alt="プロフィール画像" 
-              class="profile-image mx-3">
-
-            <i v-else 
-              class="fas fa-user-circle fa-2x mx-3"></i> <!-- ここでFontAwesomeアイコンを使用 -->
-        </div>
-          <span>{{ post.user.name}}</span>
-        </div>
+          <div class="d-flex align-items-center">
+            <div>
+              <img v-if="post.user.profile_image" 
+                  :src="`/storage/profile_images/${post.user.profile_image}`" 
+                  alt="プロフィール画像" 
+                  class="profile-image mx-3">
+              <i v-else 
+                class="fas fa-user-circle fa-2x mx-3"></i>
+            </div>
+            <span>{{ post.user.name}}</span>
+          </div>
         </div>
       </div>
       <hr>
@@ -53,7 +55,7 @@ import { ref, defineProps } from 'vue';
 import LikeButton from './LikeButton.vue';
 import SavePostButton from './SavePostButton.vue';
 
-// propsを定義
+// propsと関数を定義
 const props = defineProps({
   post: {
     type: Object,
@@ -73,17 +75,21 @@ const truncate = (text, maxLength) => {
   return text.length > maxLength ? text.substring(0, maxLength) + '…' : text;
 };
 
+// S3の画像URLを生成する関数を追加
+function getS3ImageUrl(filePath) {
+  const s3BaseUrl = 'https://eduforum-bucket.s3.amazonaws.com/';
+  return s3BaseUrl + filePath;
+}
+
 </script>
 
 <style scoped>
 .post-item {
-  /* 通常時のスタイル */
   transition: background-color 0.3s ease;
 }
 
 .post-item:hover {
-  /* ホバー時のスタイル */
-  background-color: #f5f5f5; /* 例: 薄い灰色に変更 */
-  cursor: pointer; /* マウスカーソルを指の形に変更 */
+  background-color: #f5f5f5;
+  cursor: pointer;
 }
 </style>

f1514e2029edeeddc0408cf1791dd1c203f910a7
hatai <wvnm370god@yahoo.co.jp>
2024-01-01 21:20:09 +0900

    プロバイダー削除

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,8 +18,5 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(UrlGenerator $url): void
-    {
-        $url->forceScheme('https');
-    }
+
 }

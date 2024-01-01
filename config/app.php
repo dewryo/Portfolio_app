a8e40d39e96777e20c0b60ef5551a547d97a2b0e
hatai <wvnm370god@yahoo.co.jp>
2024-01-01 19:46:35 +0900

    https通信できるようにルーティング変更

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://eduforumapp.com/posts'),
+    'url' => env('APP_URL', 'https://eduforumapp.com'),
 
     'asset_url' => env('ASSET_URL'),
 
--- routes/web.php
+++ routes/web.php
@@ -32,21 +32,17 @@ Route::get('/posts',[PostController::class, 'showhome'])->name('home');
 
 // ログインページ
 Route::get('/login', [AuthController::class, 'showLoginForm'])->middleware('guest') ->name('login');
-// ログイン処理
-Route::post('/login', [AuthController::class, 'login']);
-// ログアウト処理
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
 
 
 // 新規登録フォームの表示
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->middleware('guest') ->name('register');
-// 新規登録処理
-Route::post('/register', [RegisterController::class,'register']);
+
 
 //新規投稿フォームの表示
 Route::get('/posts/form', [PostController::class, 'showPostForm'])->middleware('auth')->name('post');
-//新規投稿処理
-Route::post('/posts', [PostController::class, 'store']);
+
 
 //投稿詳細画面の表示
 Route::get('/posts/{post}', [PostController::class, 'showPost'])->name('post.show');
@@ -54,8 +50,7 @@ Route::get('/posts/{post}', [PostController::class, 'showPost'])->name('post.sho
 
 // プロフィール編集画面
 Route::get('/profile/edit', [ProfileController::class,'edit'])->name('profile.edit');
-// プロフィール更新
-Route::put('/profile', [ProfileController::class,'update'])->name('profile.update');
+
 
 
 // 自分の投稿一覧ページ表示
@@ -66,13 +61,28 @@ Route::get('/posts/users/{id}',[PostController::class, 'user_post'])->name('user
 
 // 投稿編集ページへのルート
 Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-// 投稿更新のためのルート
-Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
-// 投稿削除のためのルート
-Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+
 
 // 保存した投稿一覧ページ表示
 Route::get('/saved_post/{id}',[PostController::class, 'saved_post'])->middleware('auth')->name('saved_post');
 
 // ゲストログイン用のルート
 Route::get('/guest-login', [LoginController::class, 'guestLogin'])->name('guest_login');
+
+Route::middleware(['forceHttps'])->group(function () {
+    // ログイン処理
+Route::post('/login', [AuthController::class, 'login']);
+// ログアウト処理
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+// 新規登録処理
+Route::post('/register', [RegisterController::class,'register']);
+//新規投稿処理
+Route::post('/posts', [PostController::class, 'store']);
+// プロフィール更新
+Route::put('/profile', [ProfileController::class,'update'])->name('profile.update');
+// 投稿更新のためのルート
+Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+// 投稿削除のためのルート
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+});

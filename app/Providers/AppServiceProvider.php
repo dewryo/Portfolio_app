deefdd820913a38d3f42f189169c4054b94691f8
hatai <wvnm370god@yahoo.co.jp>
2024-01-01 20:42:33 +0900

    routeメソッドで生成をhttps化

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,11 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+    public function boot(UrlGenerator $url)
+    {
+    $url->forceScheme('https');
+    }
     /**
      * Register any application services.
      */
